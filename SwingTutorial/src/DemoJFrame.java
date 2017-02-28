df557438c3321a9b782223ee16b88ebc2d7d75d0
Michel Bourgeois <promotions332@gmail.com>
2017-02-28 01:06:22 -0800

    add the event dispatch thread code

--- SwingTutorial/src/DemoJFrame.java
+++ SwingTutorial/src/DemoJFrame.java
@@ -3,11 +3,25 @@ import java.awt.Font;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
 
 public class DemoJFrame {
 
 	public static void main(String[] args) {
-		// Should not be in the main method
+	   // The event Dispatch Thread can not start in the main
+	   // So we in main method we need to invoke startUp method in the event Dispatch Thread
+		SwingUtilities.invokeLater(new Runnable(){
+
+			
+			public void run() {
+				new DemoJFrame().startUp();
+				
+			}
+			
+			
+		});
+	}
+		public void startUp(){
 		JLabel text= new JLabel("This is a Swing Frame", JLabel.CENTER);
 		text.setForeground(Color.RED);
 		text.setFont(new Font("serif", Font.BOLD, 42));
@@ -20,3 +34,6 @@ public class DemoJFrame {
 	}
 
 }
+// the code for the event Dispatch thread in Java 8 is simpler
+// from the java8 Lambda expression SwingUtilities.invokeLater(() -> new DemoJFrame().startUp());
+// So now the program work and does not violate the Swing single threaded rule
